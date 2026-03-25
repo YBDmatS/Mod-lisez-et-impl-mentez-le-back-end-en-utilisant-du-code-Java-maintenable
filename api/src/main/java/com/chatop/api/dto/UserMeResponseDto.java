dbfa8b1920269522142d80e82d5ef330d14ec0dc
YBDmatS <sasia.mathilde@gmail.com>
2026-03-25 15:37:50 +0100

    [FEAT] Add me endpoint (controller, service, DTO)

---
+++ api/src/main/java/com/chatop/api/dto/UserMeResponseDto.java
@@ -0,0 +1,47 @@
+package com.chatop.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * DTO for user information response.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserMeResponseDto {
+
+    /**
+     * Unique identifier of the user.
+     */
+    private long id;
+
+    /**
+     * User's full name.
+     */
+    private String name;
+
+    /**
+     * User's email address.
+     */
+    private String email;
+
+    /**
+     * Timestamp of when the user was created, formatted as "yyyy/MM/dd".
+     */
+    @JsonProperty("created_at")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd")
+    private LocalDateTime createdAt;
+
+    /**
+     * Timestamp of when the user was last updated, formatted as "yyyy/MM/dd".
+     */
+    @JsonProperty("updated_at")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd")
+    private LocalDateTime updatedAt;
+}

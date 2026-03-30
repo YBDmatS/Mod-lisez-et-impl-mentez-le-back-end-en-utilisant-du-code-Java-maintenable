a020f40c3303c8628d058e79beea7da833f239e3
YBDmatS <sasia.mathilde@gmail.com>
2026-03-30 12:13:56 +0200

    [CORE] Switch Angular proxy to real API URL via environment variable for backend debugging

--- front/src/app/features/auth/services/auth.service.ts
+++ front/src/app/features/auth/services/auth.service.ts
@@ -1,26 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { LoginRequest } from '../interfaces/loginRequest.interface';
-import { AuthSuccess  } from '../interfaces/authSuccess.interface';
-import { RegisterRequest } from '../interfaces/registerRequest.interface';
 import { User } from 'src/app/interfaces/user.interface';
+import { environment } from 'src/environments/environment.prod';
+import { AuthSuccess } from '../interfaces/authSuccess.interface';
+import { LoginRequest } from '../interfaces/loginRequest.interface';
+import { RegisterRequest } from '../interfaces/registerRequest.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private readonly baseUrl = environment.baseUrl;
+  private readonly pathService = `${this.baseUrl}messages`;
 
-  private pathService = 'api/auth';
-
-  constructor(private httpClient: HttpClient) { }
+  constructor(private readonly httpClient: HttpClient) {}
 
   public register(registerRequest: RegisterRequest): Observable<AuthSuccess> {
-    return this.httpClient.post<AuthSuccess>(`${this.pathService}/register`, registerRequest);
+    return this.httpClient.post<AuthSuccess>(
+      `${this.pathService}/register`,
+      registerRequest,
+    );
   }
 
   public login(loginRequest: LoginRequest): Observable<AuthSuccess> {
-    return this.httpClient.post<AuthSuccess>(`${this.pathService}/login`, loginRequest);
+    return this.httpClient.post<AuthSuccess>(
+      `${this.pathService}/login`,
+      loginRequest,
+    );
   }
 
   public me(): Observable<User> {

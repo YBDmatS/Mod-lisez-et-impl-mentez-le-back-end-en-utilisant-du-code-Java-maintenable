1e7dd660eaae9d53a440f9d9bcc05e0623593c87
YBDmatS <sasia.mathilde@gmail.com>
2026-03-30 12:13:56 +0200

    [CORE] Switch Angular proxy to real API URL via environment variable for backend debugging

--- front/src/app/features/rentals/services/messages.service.ts
+++ front/src/app/features/rentals/services/messages.service.ts
@@ -1,19 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { MessageRequest } from '../interfaces/api/messageRequest.interface';
 import { MessageResponse } from '../interfaces/api/messageResponse.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessagesService {
+  private readonly baseUrl = environment.baseUrl;
+  private readonly pathService = `${this.baseUrl}messages`;
 
-  private pathService = 'api/messages';
-
-  constructor(private httpClient: HttpClient) { }
+  constructor(private readonly httpClient: HttpClient) {}
 
   public send(messageRequest: MessageRequest): Observable<MessageResponse> {
-    return this.httpClient.post<MessageResponse>(this.pathService, messageRequest);
-  } 
+    return this.httpClient.post<MessageResponse>(
+      this.pathService,
+      messageRequest,
+    );
   }
+}

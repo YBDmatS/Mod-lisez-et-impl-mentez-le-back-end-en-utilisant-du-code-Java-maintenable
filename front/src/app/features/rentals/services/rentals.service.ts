a7796804081b4d03785d99826a0c9b46e6847c8a
YBDmatS <sasia.mathilde@gmail.com>
2026-03-31 11:03:15 +0200

    Revert "[FIX] Fix frontend URL used in service calls"
    This reverts commit b7d8a2a7652c1573021c6c7da8eeb948cc39922c.

--- front/src/app/features/rentals/services/rentals.service.ts
+++ front/src/app/features/rentals/services/rentals.service.ts
@@ -11,7 +11,7 @@ import { RentalsResponse } from '../interfaces/api/rentalsResponse.interface';
 })
 export class RentalsService {
   private readonly baseUrl = environment.baseUrl;
-  private readonly pathService = `${this.baseUrl}rentals`;
+  private readonly pathService = `${this.baseUrl}messages`;
 
   constructor(private readonly httpClient: HttpClient) {}
 
--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  baseUrl: 'http://localhost:8080/api/',
-  production: true,
+  baseUrl: 'localhost:3001/api/',
+  production: true
 };

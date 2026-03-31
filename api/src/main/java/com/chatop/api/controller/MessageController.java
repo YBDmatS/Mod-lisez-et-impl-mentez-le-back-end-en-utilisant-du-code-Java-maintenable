dffacf2ae5bc5b8a4c43334d523bdf820df1c067
YBDmatS <sasia.mathilde@gmail.com>
2026-03-31 13:03:09 +0200

    [FEAT] Add create message endpoint (controller, service, dto, repository)

---
+++ api/src/main/java/com/chatop/api/controller/MessageController.java
@@ -0,0 +1,34 @@
+package com.chatop.api.controller;
+
+import com.chatop.api.dto.MessageDto;
+import com.chatop.api.dto.StandardResponseDto;
+import com.chatop.api.service.MessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller responsible for handling message-related endpoints.
+ */
+@RestController
+@RequestMapping("/api/messages")
+@RequiredArgsConstructor
+public class MessageController {
+
+    public final MessageService messageService;
+
+    /**
+     * Endpoint for creating a new message.
+     *
+     * @param messageDto the message data transfer object containing the necessary information to create a new message
+     * @return HTTP 200 OK with a standard response indicating the success of the message creation
+     */
+    @PostMapping
+    public ResponseEntity<StandardResponseDto> createMessage(@RequestBody MessageDto messageDto) {
+        StandardResponseDto response = messageService.createMessage(messageDto);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ api/src/main/java/com/chatop/api/dto/MessageDto.java
@@ -0,0 +1,41 @@
+package com.chatop.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Data Transfer Object (DTO) for representing a message in the application.
+ */
+@Data
+@AllArgsConstructor
+public class MessageDto {
+
+    /**
+     * The ID of the user sending the message.
+     * This field is required and must be a positive number.
+     */
+    @NotNull(message = "User ID is required")
+    @Positive(message = "User ID must be a positive number")
+    @JsonProperty("user_id")
+    long userId;
+
+    /**
+     * The ID of the rental associated with the message.
+     * This field is required and must be a positive number.
+     */
+    @NotNull(message = "Rental ID is required")
+    @Positive(message = "Rental ID must be a positive number")
+    @JsonProperty("rental_id")
+    long rentalId;
+
+    /**
+     * The content of the message.
+     * This field is required and cannot be blank.
+     */
+    @NotBlank(message = "Message is required")
+    String message;
+}

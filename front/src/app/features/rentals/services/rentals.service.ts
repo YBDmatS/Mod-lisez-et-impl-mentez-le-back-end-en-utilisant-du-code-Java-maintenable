bbc329bf3a1e6eecab6b103b35364cd4f4d346b1
YBDmatS <sasia.mathilde@gmail.com>
2026-03-31 11:03:33 +0200

    Revert "[CORE] Switch Angular proxy to real API URL via environment variable for backend debugging"
    This reverts commit a020f40c3303c8628d058e79beea7da833f239e3.

--- front/src/app/features/rentals/services/rentals.service.ts
+++ front/src/app/features/rentals/services/rentals.service.ts
@@ -2,18 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Rental } from 'src/app/features/rentals/interfaces/rental.interface';
-import { environment } from 'src/environments/environment';
 import { RentalResponse } from '../interfaces/api/rentalResponse.interface';
 import { RentalsResponse } from '../interfaces/api/rentalsResponse.interface';
 
+
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class RentalsService {
-  private readonly baseUrl = environment.baseUrl;
-  private readonly pathService = `${this.baseUrl}messages`;
 
-  constructor(private readonly httpClient: HttpClient) {}
+  private pathService = 'api/rentals';
+
+  constructor(private httpClient: HttpClient) { }
 
   public all(): Observable<RentalsResponse> {
     return this.httpClient.get<RentalsResponse>(this.pathService);
@@ -24,13 +24,10 @@ export class RentalsService {
   }
 
   public create(form: FormData): Observable<RentalResponse> {
-    return this.httpClient.post<RentalResponse>(`${this.pathService}`, form);
+    return this.httpClient.post<RentalResponse>(this.pathService, form);
   }
 
   public update(id: string, form: FormData): Observable<RentalResponse> {
-    return this.httpClient.put<RentalResponse>(
-      `${this.pathService}/${id}`,
-      form,
-    );
+    return this.httpClient.put<RentalResponse>(`${this.pathService}/${id}`, form);
   }
 }
